7a69c7d26c575a084dded9a547b8ba6ec1e7f4e9
Amil Masic <amil.masic@gmail.com>
2019-04-03 09:01:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -56,9 +56,9 @@ class Dog
   end
 
   def self.new_from_db(row)
-    :id = row[0]
-    :name = row[1]
-    :breed = row[2]
-    self.new(id:, name:, breed:)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id,name: name,breed: breed)
   end
 end
