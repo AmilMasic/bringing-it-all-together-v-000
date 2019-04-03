8dd80ab049b3abc9187bd87d9c374958b4ac672d
Amil Masic <amil.masic@gmail.com>
2019-04-03 07:48:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,8 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(id = nil, name, breed)
-    @name = name
-    @breed = breed
-    @id = id
+  def initialize(h)
+    h.each {|k,v| public_send("#{k}=",v)}
   end
 
 
